b7ab2d3e81ae615b92af6515b7acdb71721ae2cd
andrewcgaitskell <andrew@gaitskell.com>
2024-08-17 11:43:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import matplotlib.patches as mpatches
 import matplotlib.lines as mlines
 import numpy as np
 
-from blueprint.standard import standard_bp
+from blueprints.standard import standard_bp
 
 app = Flask(__name__)
 
