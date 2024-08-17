ed28a682ecee0cc3bc871c1894fd9d94ca9fce89
andrewcgaitskell <andrew@gaitskell.com>
2024-08-17 11:50:01 +0100

    Update standard.py

--- blueprints/standard.py
+++ blueprints/standard.py
@@ -5,12 +5,12 @@ standard_bp = Blueprint('main', __name__)
 # Define routes for the blueprint
 @standard_bp.route('/help')
 def dmtools_help():
-    return render_template('main.dmtools_help.html')
+    return render_template('dmtools_help.html')
 
 @standard_bp.route('/faq')
 def dmtools_faq():
-    return render_template('main.dmtools_faq.html')
+    return render_template('dmtools_faq.html')
 
 @standard_bp.route('/contact')
 def dmtools_contact():
-    return render_template('main.dmtools_contact.html')
+    return render_template('dmtools_contact.html')
