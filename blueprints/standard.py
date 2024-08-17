cb9cacc9de3f8f1ba100dcc25846b945a37e49f3
andrewcgaitskell <andrew@gaitskell.com>
2024-08-17 11:40:30 +0100

    Create standard.py

---
+++ blueprints/standard.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, render_template
+
+standard_bp = Blueprint('main', __name__)
+
+# Define routes for the blueprint
+@standard_bp.route('/help')
+def dmtools_help():
+    return render_template('dmtools_help.html')
+
+@standard_bp.route('/faq')
+def dmtools_faq():
+    return render_template('dmtools_faq.html')
+
+@standard_bp.route('/contact')
+def dmtools_contact():
+    return render_template('dmtools_contact.html')
