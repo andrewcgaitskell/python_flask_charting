d3b699752b0e4addf433229ec18b24d0ada827c3
andrewcgaitskell <andrew@gaitskell.com>
2024-08-24 10:02:21 +0100

    Update standard.py

--- blueprints/standard.py
+++ blueprints/standard.py
@@ -1,20 +1,22 @@
 from flask import Blueprint, render_template
 
-standard_bp = Blueprint('main', __name__)
+standard_bp = Blueprint('standard_bp', __name__)
+
+# Define routes for the blueprint
+@standard_bp.route('/home')
+def dmtools_help():
+    return render_template('static/home.html')
 
 # Define routes for the blueprint
 @standard_bp.route('/help')
 def dmtools_help():
-    return render_template('dmtools_help.html')
+    return render_template('static/dmtools_help.html')
 
 @standard_bp.route('/faq')
 def dmtools_faq():
-    return render_template('dmtools_faq.html')
+    return render_template('static/dmtools_faq.html')
 
 @standard_bp.route('/contact')
 def dmtools_contact():
-    return render_template('dmtools_contact.html')
+    return render_template('static/dmtools_contact.html')
 
-@standard_bp.route('/offcanvas')
-def offcanvas():
-    return render_template('offcanvas.html')
