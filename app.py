d285d8c812588bd01facc937cef3cd35d78904c8
andrewcgaitskell <andrew@gaitskell.com>
2024-08-27 12:01:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -23,6 +23,7 @@ from blueprints.standard import standard_bp
 from blueprints.charts import charts_bp
 from blueprints.data import data_bp
 from blueprints.viewing import viewing_bp
+from blueprints.forms import forms_bp
 
 app = Flask(__name__, static_folder='static')
 
@@ -30,6 +31,7 @@ app.register_blueprint(standard_bp, url_prefix="/")
 app.register_blueprint(charts_bp, url_prefix="/charts")
 app.register_blueprint(data_bp, url_prefix="/data")
 app.register_blueprint(viewing_bp, url_prefix="/viewing")
+app.register_blueprint(forms_bp, url_prefix="/forms")
 
 @app.route('/')
 def home():
