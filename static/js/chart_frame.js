c2463c29d5a845e13464860160696e4ff4481050
andrewcgaitskell <andrew@gaitskell.com>
2024-08-30 14:47:00 +0100

    Update chart_frame.js

--- static/js/chart_frame.js
+++ static/js/chart_frame.js
@@ -3,15 +3,15 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Get the value of the CSS variable
     const root = document.documentElement;
-    const mainColor = getComputedStyle(root).getPropertyValue('----screen-width').trim();
+    const screenWidth = getComputedStyle(root).getPropertyValue('----screen-width').trim();
 
     // Optional: Send the value to the Flask server
-    fetch('/receive_css_variable', {
+    fetch('/charts/receive_css_variable', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ mainColor: mainColor })  // Send the CSS variable value as JSON
+        body: JSON.stringify({ screenWidth: screenWidth })  // Send the CSS variable value as JSON
     })
     .then(response => response.json())
     .then(data => {
