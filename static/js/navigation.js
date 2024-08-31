9287dbaf904b5964a4a5a226dc7ed78104ee6d78
andrewcgaitskell <andrew@gaitskell.com>
2024-08-31 09:56:09 +0100

    Create navigation.js

---
+++ static/js/navigation.js
@@ -0,0 +1,12 @@
+ function togglePanel() {
+        var panel = document.getElementById("sidePanel");
+        var mainContent = document.getElementById("main-content");
+
+        if (panel.style.left === "-250px") {
+            panel.style.left = "0";
+            mainContent.style.marginLeft = "250px";
+        } else {
+            panel.style.left = "-250px";
+            mainContent.style.marginLeft = "0";
+        }
+    }
