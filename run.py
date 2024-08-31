9f61dc4b7937a60a858bdd3dbe60bf5c5dfb1f13
andrewcgaitskell <andrew@gaitskell.com>
2024-08-31 16:40:29 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
-from app import create_app
+from app import init_app
 
-app = create_app()
+application = init_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    application.run(debug=True)
